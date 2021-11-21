be40a8ebd196d7052f590ae58c1bf2b3f6fb9256
Laura <laura.robertson64@gmail.com>
2021-11-21 00:01:44 -0600

    batman and cactus

--- src/asciiLib.ts
+++ src/asciiLib.ts
@@ -328,6 +328,36 @@ const toTheMoon = (): Array<string> => {
   return toTheMoonBlock;
 };
 
+const cactus = (): Array<string> => {
+  const cactusBlock: string[] = [
+    "   ,*-.",
+    "    |  |",
+    ",.  |  |",
+    "| |_|  | ,.",
+    "`---.  |_| |",
+    "    |  .--`",
+    "    |  |",
+    "    |  | kra",
+  ];
+  return cactusBlock;
+};
+
+const batman = (): Array<string> => {
+  const batmanBlock: string[] = [
+    "         _,    _   _    ,_",
+    "    .o888P     Y8o8Y     Y888o.",
+    "   d88888      88888      88888b",
+    "  d888888b_  _d88888b_  _d888888b",
+    "  8888888888888888888888888888888",
+    "  8888888888888888888888888888888",
+    "  YJGS8P\"Y888P\"Y888P\"Y888P\"Y8888P",
+    "   Y888   '8'   Y8P   '8'   888Y",
+    "    '8o          V          o8'",
+    "      `                     `",
+  ];
+  return batmanBlock;
+};
+
 const template = (): Array<string> => {
   const Block: string[] = [
     ""
@@ -355,6 +385,8 @@ export const asciiLib = {
   moon,
   ufo,
   toTheMoon,
+  cactus,
+  batman,
 };
 
 
