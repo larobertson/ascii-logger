973d9568d9370ddfe27526d922ab47bbdf3b0c8d
Laura <laura.robertson64@gmail.com>
2021-11-20 23:31:38 -0600

    more art for lib

--- src/asciiLib.ts
+++ src/asciiLib.ts
@@ -4,7 +4,7 @@
 
 const owl = (): Array<string> => {
   const owlBlocks: string[] = [
-    " /\\_/\\ ",
+    "/\\_/\\ ",
     "((@v@))",
     "():::()",
     " VV-VV ",
@@ -58,17 +58,226 @@ const cowWithBell = (): Array<string> => {
     "          /(O) \\\\   _,.-.,_)",
     "          //  \\\\ `\\'`      /",
     "    jgs  / |  ||   `\"\"\"\"~\"`",
-    "      /'  |__||",
-    "            `o",
+    "       /'  |__||",
+    "             `o",
   ];
   return cowWithBellBlocks;
-}
+};
+
+const dog = (): Array<string> => {
+  const dogBlocks: string[] = [
+    "         _=,_",
+    "      o_/6 /#\\",
+    "      \\__ |##/",
+    "      ='|--\\",
+    "        /   #'-.",
+    "        \\#|_   _'-. /",
+    "        |/ \\_( # |\" ",
+    "snd    C/ ,--___/",
+  ];
+  return dogBlocks;
+};
+
+const greatDane = (): Array<string> => {
+  const greatDaneBlocks: string[] = [
+    " __/ / \\",
+    "|    |/\\",
+    "|_--\\   \\              /-",
+    "     \\   \\-___________/ /",
+    "      \\                :",
+    "      |                :",
+    "      |       ___ \\    )",
+    "       \\|  __/     \\  )",
+    "        | /         \\  \\",
+    "        |l           ( l",
+    "        |l            ll",
+    "        |l            |l",
+    "       / l           / l",
+    "       --/           --"
+  ];
+  return greatDaneBlocks;
+};
+
+const sleepyCat = (): Array<string> => {
+  const sleepyCatBlocks: string[] = [
+    "      |\\      _,,,---,,_",
+    "ZZZzz /,`.-'`'    -.  ;-;;,_",
+    "     |,4-  ) )-,_. ,\\ (  `'-'",
+    "    '---''(_/--'  `-'\\_)  Felix Lee ",
+  ];
+  return sleepyCatBlocks;
+};
+
+const bee = (): Array<string> => {
+  const beeBlocks: string[] = [
+    "Art by Joan Stark",
+    "                .' '.            __",
+    "       .        .   .           (__\\_",
+    "        .         .         . -{{_(|8)",
+    "jgs       ' .  . ' ' .  . '     (__/",
+  ];
+  return beeBlocks;
+};
+
+const scorpion = (): Array<string> => {
+  const scorpionBlocks: string[] = [
+    "Art by Max Strandberg",
+    " ___    ___",
+    "( _<    >_ )",
+    "//        \\\\",
+    "\\\\___..___//",
+    " `-(    )-'",
+    "   _|__|_",
+    "  /_|__|_\\",
+    "  /_|__|_\\",
+    "  /_\\__/_\\",
+    "   \\ || /  _)",
+    "     ||   ( )",
+    "Max  \\\\___//",
+    "      `---'",
+  ];
+  return scorpionBlocks;
+};
+
+const drum = (): Array<string> => {
+  const drumBlock: string[] = [
+  "  ======o     o======",
+  "       ___________",
+  "      |___________|  ",
+  "       |\\  /\\  /\\|",
+  "       |_\\/__\\/__|",
+  "      |___________| AH",
+  ];
+  return drumBlock;
+};
+
+
+const trumpet = (): Array<string> => {
+  const trumpetBlock: string[] = [
+  "                     /|",
+  "       =  =  =      / |",
+  "  ____| || || |____/  | -_-_-_-_-_-_",
+  "|)----| || || |____   |     AH",
+  "  ((  | || || |  ))\\  | _-_-_-_-_-_-",
+  "  \\\\_|_||_||_|_//  \\ |",
+  "    \\___________/    \\|",
+  ];
+  return trumpetBlock;
+};
+
+const guitar = (): Array<string> => {
+  const guitarBlock: string[] = [
+  "       ___",
+  "     o|* *|o",
+  "     o|* *|o",
+  "     o|* *|o",
+  "      \\===/",
+  "       |||",
+  "       |||",
+  "       |||",
+  "       |||",
+  "    ___|||___",
+  "   /   |||   \\",
+  "  /    |||    \\",
+  " |     |||     |",
+  "  \\   (|||)   /",
+  "   |   |||   |",
+  "  /    |||    \\",
+  " /     |||     \\",
+  "/      |||      \\",
+  "|     [===]     |",
+  " \\             /",
+  "  '.         .'",
+  "    '-------'",
+  ];
+  return guitarBlock;
+};
+
+const alleyCats = (): Array<string> => {
+  const alleyCatsBlock: string[] = [
+  "                     /^--^\\     /^--^\\     /^--^\\",
+  "                      \\____/     \\____/     \\____/",
+  "                     /      \\   /      \\   /      \\",
+  "                    |        | |        | |        |",
+  "                     \\__  __/   \\__  __/   \\__  __/",
+  "|^|^|^|^|^|^|^|^|^|^|^|^\\ \\^|^|^|^/ /^|^|^|^|^\\ \\^|^|^|^|^|^|^|^|^|^|^|^|",
+  "| | | | | | | | | | | | |\\ \\| | |/ /| | | | | | \\ \\ | | | | | | | | | | |",
+  "| | | | | | | | | | | | / / | | |\\ \\| | | | | |/ /| | | | | | | | | | | |",
+  "| | | | | | | | | | | | \\/| | | | \\/| | | | | |\\/ | | | | | | | | | | | |",
+  "#########################################################################",
+  "| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |",
+  "| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |",
+  ];
+  return alleyCatsBlock;
+};
+
+const cat = (): Array<string> => {
+  const catBlock: string[] = [
+  "  Art by Blazej Kozlowski",
+  "      _                        ",
+  "      \\`*-.                    ",
+  "       )  _`-.                 ",
+  "      .  : `. .                ",
+  "      : _   '  \\               ",
+  "      ; *` _.   `*-._          ",
+  "      `-.-'          `-.       ",
+  "        ;       `       `.     ",
+  "        :.       .        \\    ",
+  "        . \\  .   :   .-'   .   ",
+  "        '  `+.;  ;  '      :   ",
+  "        :  '  |    ;       ;-. ",
+  "        ; '   : :`-:     _.`* ;",
+  "[bug] .*' /  .*' ; .*`- +'  `*' ",
+  "      `*-*   `*-*  `*-*'",
+  ];
+  return catBlock;
+};
+
+const camel = (): Array<string> => {
+  const camelBlock: string[] = [
+  "   _______\\\\__",
+  "  (_. _ ._  _/",
+  "   '-' \\__. /",
+  "        /  /",
+  "       /  /    .--.  .--.",
+  "      (  (    / '' \\/ '' \\   \"",
+  "       \\  \\_.'            \\   )",
+  "       ||               _  './",
+  "        |\\   \\     ___.'\\  /",
+  "          '-./   .'    \\ |/",
+  "             \\| /       )|\\",
+  "              |/       // \\\\",
+  "              |\\    __//   \\\\__",
+  "             //\\\\  /__/  mrf\\__|",
+  "         .--_/  \\_--.",
+  "        /__/      \\__\\",
+  ];
+  return camelBlock;
+};
+
+const template = (): Array<string> => {
+  const Block: string[] = [
+    ""
+  ];
+  return Block;
+};
 
 export const asciiLib = {
   owl,
   eagle,
   cow,
   cowWithBell,
+  dog,
+  greatDane,
+  bee,
+  scorpion,
+  drum,
+  trumpet,
+  guitar,
+  alleyCats,
+  sleepyCat,
+  cat,
+  camel, 
 };
 
 
